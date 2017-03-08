9d56be98a68554a9989ea743ca00e1ab4296430e
fenrisf <fenrisf@gmail.com>
2017-03-08 16:11:29 +0100

    Primeros pasos

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by albertomartinez on 8/03/17.
  */
-@Controller
 public interface MainController {
+    String getHelloWold(String name);
 }
