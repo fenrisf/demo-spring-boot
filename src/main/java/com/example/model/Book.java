1c9ee211652222c1f9aaf448eb54395aa548410a
fenrisf <fenrisf@gmail.com>
2017-03-09 15:56:20 +0100

    Some addings
    Adding tests to learn how test in spting boot
    Adding data management with spring Data

---
+++ src/main/java/com/example/model/Book.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by albertomartinez on 9/03/17.
+ */
+@Entity
+@Data
+public class Book {
+    private static final long serialVersionUID = 1L;
+    @Id
+    long id;
+    @Column(name="name")
+    String name;
+    @Column(name="author")
+    String author;
+    @Column(name="price")
+    double price;
+
+}
---
+++ src/main/java/com/example/service/BookService.java
@@ -0,0 +1,42 @@
+package com.example.service;
+
+import com.example.model.Book;
+import com.example.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private BookRepository repository;
+
+    @Autowired
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public Iterable<Book> findByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public Iterable<Book> findAll() {
+        return  repository.findAll();
+    }
+
+    public Book addBook(Book book){
+        return repository.save(book);
+    }
+
+    public Iterable<Book> addAll (Iterable<Book> books){
+        return repository.save(books);
+    }
+
+    public Iterable<Book> findByNameAndAuthor(String name, String author) {
+        return repository.findByNameAndAuthor(name,author);
+    }
+
+    public Iterable<Book> findByPrice(double price) {
+        return repository.findByPrice(price);
+    }
+}
